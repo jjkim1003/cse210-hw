a12c0849103d1fed96546de10988814e2b45521c
jjkim1003 <jaejinkim@gmail.com>
2025-01-07 22:54:52 -0600

    updated assign 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Please enter your grade percentage. ");
+        string gradeFromStudent = Console.ReadLine();
+
+        int x = int.Parse(gradeFromStudent);
+
+        if (x >= 90)
+        {
+            Console.WriteLine ("Your current grade is A. Congraulations!");
+        }
+        else if (x >= 80)
+        {
+            Console.WriteLine ("Your current grade is B. Congraulations!");
+        }
+        else if (x >= 70)
+        {
+            Console.WriteLine ("Your current grade is C. Congraulations!")
+        }
+        else if (x >= 60)
+        {
+            Console.WriteLine ("Your current grade is D. You can pass the course (above C) if you study a little more. ")
+        }
+        else (x < 60)
+        {
+            Console.WriteLine ("Your current grade is F. You can pass the course (above C) if you study harder. ")
+        }
+
+
     }
 }
